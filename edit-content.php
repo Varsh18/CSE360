be895d66953b827b8928d68d611227f6104b2053
Varsh18 <varshiniravi1824@gmail.com>
2019-06-04 13:33:05 +0530

    add form for insert

--- edit-content.php
+++ edit-content.php
@@ -3,6 +3,31 @@
   <head>
     <meta charset="utf-8">
     <title>Our Contents</title>
+    <style>
+    #content-box{
+      width:100%;
+      display:flex;
+      flex-wrap: wrap;
+    }
+    .box{
+      margin-left: 3em;
+      margin-top:  2em;
+      padding: 1.5em;
+      text-align: center;
+    }
+    .desc{
+      padding-top: 1.5em;
+      display:inline-block;
+    }
+    a{
+      text-decoration: none;
+      font-size: 2em;
+    }
+    img{
+      width:200px;
+      height:150px;
+    }
+    </style>
   </head>
   <body>
     <div id="content-box">
@@ -14,6 +39,19 @@
                 <a href="#"><span> Add new </span></a>
             </div>
         </div>
+    </div>
+    <div id="add-content">
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="POST" enctype="multipart/form-data">
+          <div id="insert-image">
+          <input type="text" name="name" placeholder="Enter the content name"/>
+          </div>
+          <div id="insert-image">
+              <input type="file" name="file" accept="image/gif, image/jpeg" />
+          </div>
+          <div id="form-submit">
+              <input type="submit" name="submit" value="Create"/>
+          </div>
+      </form>
     </div>
   </body>
 </html>
