bf6a3a1db927f9cb907a75084af55f7b47185951
Varsh18 <varshiniravi1824@gmail.com>
2019-06-04 12:52:18 +0530

    Add new option in edit portal

--- edit-content.php
+++ edit-content.php
@@ -5,6 +5,15 @@
     <title>Our Contents</title>
   </head>
   <body>
-
+    <div id="content-box">
+        <div class="box">
+            <div class="picture">
+                <a href="#"><img src="images/add_new.png" alt="add new"/></a>
+            </div>
+            <div class="desc">
+                <a href="#"><span> Add new </span></a>
+            </div>
+        </div>
+    </div>
   </body>
 </html>
