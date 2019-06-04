664024042cdaa6df6db25ab06b8b6ed6e73a0fd2
Varsh18 <varshiniravi1824@gmail.com>
2019-06-04 11:56:21 +0530

    Add user image in admin page

---
+++ admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Admin Console</title>
+  </head>
+  <body>
+ <div id="container">
+      <div class="box">
+            <div class="picture">
+                  <a href="#"><img src="images/user.png" alt="user-icon"/></a>
+            </div>
+            <div class="desc">
+            </div>
+      </div>
+      <div class="box">
+            <div class="picture">
+            </div>
+            <div class="desc">
+            </div>
+      </div>
+ </div>
+  </body>
+</html>
