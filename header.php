03c0480395100ad7850c51e1450bd76031846782
Varsh18 <varshiniravi1824@gmail.com>
2019-06-06 12:36:21 +0530

    add content to header.php

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div id="header">
+       <div id="content">
+         <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Content</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+       </div>
+    </div>
+  </body>
+</html>
