4be65cb2b6b6b8473f75553cf127fb6ea5faaf26
Varsh18 <varshiniravi1824@gmail.com>
2019-06-06 18:28:21 +0530

    Add copyright

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div id="foo-line">
+    <p id="copyright">Copyright ©2018 All rights reserved </p>
+    <p id="foo-right">Terms and Conditions | Register | Privacy<p>
+    </div>
+  </body>
+</html>
